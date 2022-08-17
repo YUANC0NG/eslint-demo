89f48d4f1fb73d88e11137922342b778b5aac5d1
yuancong <aiidyc@qq.com>
2022-08-17 14:21:51 +0800

    1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,22 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node:true
+    node: true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
-  plugins: ['react'],
-  rules: {}
-}
+  plugins: ["react"],
+  // 指定React版本号为自动检测 否则会提示警告
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  rules: {
+    semi: ["error", "always"]
+  }
+};
