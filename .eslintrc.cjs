d312e00bb39641a5797c7bec93d4a03c11cf838b
yuancong <aiidyc@qq.com>
2022-08-17 15:45:02 +0800

    feat(数据层): 测试描述
    没有详细描述

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,20 +4,20 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module"
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  plugins: ["react"],
+  plugins: ['react'],
   // 指定React版本号为自动检测 否则会提示警告
   settings: {
     react: {
-      version: "detect"
+      version: 'detect'
     }
   },
   rules: {
-    semi: ["error", "always"]
+    semi: ['error', 'always']
   }
 };
